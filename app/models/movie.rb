d53b29b18b2b9bc6e35a4363e21319ea511ed700
Begana Choi <begana001.jpg@gmail.com>
2022-02-24 18:14:29 +0100

    add validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,9 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
+
+  validates :title, presence: true
+  validates :overview, presence: true
+  validates :like, presence: true
+  validates :rating, presence: true
+  validates :poster_url, presence: true
 end

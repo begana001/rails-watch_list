c64604c12ac426e3a119ae9aff035202cc799fb2
Begana Choi <76692843+begana001@users.noreply.github.com>
2022-02-25 15:45:07 +0100

    add movie show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'lists#home'
-  resources :movies, only: [:index]
+  resources :movies, only: [:index, :show]
   resources :lists do
     resources :bookmarks
   end
